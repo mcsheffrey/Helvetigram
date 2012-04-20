63c7c9e79742aea3f190d8b934b3f830000562d1
Connor McSheffrey <connor.mcsheffrey@gmail.com>
2012-04-19 18:59:20 -0700

    flippin functionality working

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-/* Author: Connor McSheffrey
+// Connor McSheffrey
+// @mcsheffrey
 
-*/
 
 (function() {
 
@@ -12,29 +12,69 @@
 			this.container = config.container;
 			this.photoDimension = config.photoDimension;
 
-			this.fetch();
-			this.flip();
+			this.refresh();
 		},
 
+		// Compile the handlebars template
 		attachTemplate: function() {
 			var template = Handlebars.compile( this.template );
 
-			this.container.append( template( this.grams ) );
-
-		},
-
-		flip: function() {
-			console.log(this.grams);
+			this.container.append( template( this.grams ) );			
 			
 		},
 
-		fetch: function() {
+		refresh: function() {
 			var self = this;
 
-			$.getJSON( this.url, function( data ) {
+			this.fetch().done(function(results) {
+
+				self.buildFrag(results);
+				self.attachTemplate();
+				self.flip();
+
+				$('img').lazyload({ 
+	    		effect : "fadeIn"
+	 			});
 				
-				
-				self.grams = $.map( data.data, function( gram ) {
+			});
+		},
+
+		shuffle: function(array) {
+    	var tmp, current, top = array.length;
+	
+    	if(top) while(--top) {
+    	    current = Math.floor(Math.random() * (top + 1));
+    	    tmp = array[current];
+    	    array[current] = array[top];
+    	    array[top] = tmp;
+    	}
+	
+    	return array;
+		},
+
+		flip: function() {
+
+			self = this;
+
+			var photoContainers = $('#main').children('.photo-container'),
+					index = 0;
+			this.shuffle(photoContainers);
+
+			photoContainers.each(function(i) {
+			  setTimeout(function () {
+    			$(photoContainers[index]).addClass('flip');
+    			console.log($(photoContainers[index]));
+    			
+   	 			index++;
+				}, i*4000);
+			});
+
+		},
+
+		buildFrag: function(results) {
+			var self = this;
+
+			this.grams = $.map( results.data, function( gram ) {
 					return {
 						image: gram.images.low_resolution.url,
 						id: gram.id,
@@ -43,13 +83,12 @@
 					};
 				});
 
-				// For future lessons, research $.deferred, viewers. :)
-				self.attachTemplate(); 
-				self.flip();
+		},
 
-				$('img').lazyload({ 
-	    		effect : "fadeIn"
-	 			});
+		fetch: function() {
+			return $.ajax({
+				url: this.url,
+				dataType: 'jsonp'
 			});
 		}
 	};
@@ -61,102 +100,4 @@
 	});
 
 })();
-
-// (function($) {
-// 	var opts = {
-// 		color: '#fff'
-// 	},
-// 	spinner = new Spinner(opts).spin();
-// 	$('#main').append(spinner.el);
-
-// 	var clientID = '5ee7e77d7b0b441f9cd307a5f30c92bb',
-// 			containerWidth = $('#main').width(),
-// 			photoDimension = containerWidth/4;
-
-// 			console.log(containerWidth);
-
-
-			
-
-//   $.ajax({
-//     url: 'https://api.instagram.com/v1/media/popular?client_id=' + clientID + '&callback=callbackFunction',
-//     type: 'GET',
-//     dataType: 'jsonp',
-//     data: {param1: 'value1'},
-//     complete: function(xhr, textStatus) {
-//       //called when complete
-//     },
-//     success: function onPhotoLoaded(data) {
-
-//     		console.log(data);
-
-//     		var photos = data.data,
-//     				photoLength = photos.length;
-
-    		
-//         if(data.meta.code == 200) {
-            
-//             if(photoLength > 0) {
-//                 for (var i = 0; i <= photos.length/2; i++) {
-                    
-//                     var str = '<img id="' + data.data[i].id + '" data-original="' + data.data[i].images.low_resolution.url + '" height="' + photoDimension + '" width="' + photoDimension + '" src="img/blank.gif" />';
-//                     $('<div></div>').addClass('photo').html(str).appendTo('#main');
-
-//                     spinner.stop();
-
-//                     $('img').lazyload({ 
-//     									effect : "fadeIn"
-// 										});
-
-// 										console.log('1' + data.data[i].id );
-
-//                 }
-
-//                 for (var i = photoLength/2; i <= photoLength; i++) {
-//                 	photos = data.data
-
-//                 	console.log(i);
-
-//                 	console.log(data[i]);
-
-//                 	var str = '<img class="reverse" id="' + photos[i].id + '" data-original="' + data.data[i].images.low_resolution.url + '" height="' + photoDimension + '" width="' + photoDimension + '" src="img/blank.gif" />';
-
-//                 	$('#main img:eq(data.data[i]').append(str);
-
-//                 	console.log('2' + data.data[i].id);
-                	
-//                 }
-//             }else{
-//                 alert('empty');
-//             }
-            
-//         }else{
-//             alert(data.meta.error_message);
-//         }
-//     },
-//     error: function(xhr, textStatus, errorThrown) {
-//       //called when there is an error
-//     }
-//   });
-  
-// })(jQuery); 
-
-
-// var access_token = '3794301.f59def8.e08bcd8b10614074882b2d1b787e2b6f';
-
-//     loadFeed();
-
-//     function loadFeed() {
-//         var param = {access_token:access_token};
-//         cmd(param, onPhotoLoaded);
-//     }
-
-//     function cmd(param, callback) {
-//         //popular
-//         var cmdURL = 'https://api.instagram.com/v1/media/popular?callback=?';
-//         $.getJSON(cmdURL, param, callback);
-//     }
-// for (var i = 0; i <= data.data.length; i++) {
-//     		$('#main').append('<img src="' + data.data[i].images.low_resolution.url + '" />')
-//     	};
     
